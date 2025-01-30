1c286fa49b9f9c76c9a819b33f980250b0e1295c
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-01-30 19:35:03 +0530

    ...

--- React/tailwind.config.js
+++ React/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#3498db",
+        secondary: "#e74c3c",
+        success: "#2ecc71",
+        info: "#34495e",
+        warning: "#f1c40f",
+        danger: "#e74c3c",
+        light: "#f9f9f9",
+        dark: "#34495e",
+      }
+    },
   },
   darkMode : "class",
   plugins: [],
