20b2cdc09921f40de066f4884f39447734dc5608
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-02-09 19:10:33 +0530

    ....

--- js/object.js
+++ js/object.js
@@ -12,3 +12,19 @@ Object.setPrototypeOf(obj, prototypeObj); //?
   
 console.log(obj.getValue()); // Output: 42
   
+
+
+//keys in action
+
+const objs = {
+  india:{
+    Haryana:["faridabad","Rohtak","Hisar"],
+  },
+  US:{
+    California:["San Francisco","Los Angeles","Sacramento"],
+  }
+};
+
+const states = Object.keys(objs["india"]);
+const cities = objs["india"]?.["Haryana"];
+console.log(cities);
