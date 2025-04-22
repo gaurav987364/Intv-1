6abb7fa4e985b97e3a728a045e5cd42df4b6fc91
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-04-22 22:30:05 +0530

    Add detailed comments and examples for the scoreOfParenthesis function

--- js/DSA-Topics.js
+++ js/DSA-Topics.js
@@ -1,1 +1,34 @@
 // All about DSA Topics used in Web Dev. For Now!
+
+// *Merge two sorted Array,strings & lists;
+// *Valid Parenthesis :- There are two common ways to solve this in O(n) time and O(n) space:
+
+//     1.parenthesis checker;
+
+//     2.score of parenthesis
+//     Write a function scoreOfParentheses(s) that, given a balanced     parentheses string s, returns its “score” defined by:
+// () has score 1.
+// AB (concatenation) has score A + B.
+// (A) has score 2 × A.
+//?Sol:-  "(" par naya frame push hota hai, ")" par pop karke calculate karke add karte hain. () ke andar kuch nahi, to score = 1. (A) me A ka score agar non-zero hai, to double kar do (2×A).
+function scoreOfParenthesis(s) {
+    let stack = [0];
+
+    for(let char of s){
+        if(char === "("){
+            stack.push(0);  // pushing one frame
+        } else {
+            const score = stack.pop();  //end one
+            const currentscore = Math.max(2*score, 1);
+            stack[stack.length - 1] += currentscore;
+        }
+    }
+    return stack.pop();
+};
+console.log(scoreOfParenthesis("()")); //1
+console.log(scoreOfParenthesis("(())")); //2
+console.log(scoreOfParenthesis("(()())")); //4
+console.log(scoreOfParenthesis("(()(()))")); //6
+console.log(scoreOfParenthesis("((())(()))")); //8
+console.log(scoreOfParenthesis("(((((())))))")); //32
+console.log(scoreOfParenthesis("((())(())(())(()))")); //16
