920f70f2d8495b2b12b85366ed51fe9d613a212e
gaurav987364 <sharmagourav9654@gmail.com>
2025-07-29 23:44:38 +0530

    fisrt question capitalise firt letter

--- js/Practice.js
+++ js/Practice.js
@@ -4,3 +4,9 @@ function Practice() {
 Practice();
 
 // This function initializes the Practice module and logs a message to the console.
+
+function capitalizeFirstLetter(str){
+    const strArray = str.split(' ');
+    const capitalizeArray = strArray?.map(word => word.charAt(0).toUpperCase()+word.slice(1));
+    return capitalizeArray.join(' ');
+};
