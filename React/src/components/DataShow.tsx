a2b7b1c3e84a06b54a776df5a28d15fef6af9fd8
gaurav987364 <sharmagourav9654@gmail.com>
2025-12-16 23:04:22 +0530

    c4

--- React/src/components/DataShow.tsx
+++ React/src/components/DataShow.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DataShow = ({data}) => {
   return (
     <div className=' flex items-center justify-start w-full h-auto bg-gray-300'>
-        {data?.map((item:any, index) => (
+        {data?.map((item:any, index:) => (
             <p key={item.id}>{item.id}</p>
         ))}
     </div>
