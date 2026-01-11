df5413e69ddfdab22f66c22e1e78bb204868a896
gaurav987364 <sharmagourav9654@gmail.com>
2026-01-11 18:22:12 +0530

    c4

--- js/string.js
+++ js/string.js
@@ -25,7 +25,7 @@ function capitalize(str) {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 }
-console.log(capitalize("Hey My Name Is gaurav. how are you bro."));
+console.log(capitalize("Hey My Name Is Gaurav. how are you bro."));
 //Hey My Name Is Gaurav. How Are You Bro.
 
 // Q:Reverse a String✅
