977de6267151c1b5a8fe148eacfed9ea9c5520df
gaurav987364 <sharmagourav9654@gmail.com>
2026-01-14 22:59:49 +0530

    write solution to quedstion

--- js/date.js
+++ js/date.js
@@ -4,3 +4,13 @@
 
 // Q:- write a function that input date and count the age?
 // solution:-
+function calculateAge(birthDate) {  
+    const today = new Date();
+    const birth = new Date(birthDate);
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDiff = today.getMonth() - birth.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+        age--;
+    }
+    return age;
+}
